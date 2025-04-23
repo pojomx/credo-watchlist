b93f1fa3f29cd6a1c2cf126664630dcdb1e0edba
Alan Milke <pojo@pojoclan.com>
2025-04-22 18:27:10 -0600

    Initial Commit

---
+++ credo-watchlist/credo_watchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  credo_watchlistApp.swift
+//  credo-watchlist
+//
+//  Created by Alan Milke on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct credo_watchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
